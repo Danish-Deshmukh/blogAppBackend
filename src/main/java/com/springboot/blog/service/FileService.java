707580480a4197fde59e6aecd8d5c129f4487b75
Deshmukh Danish <dd39775@gmail.com>
2024-06-26 11:43:40 +0530

    add the FileServe class to proper location

--- src/main/java/com/springboot/blog/service/FileService.java
+++ src/main/java/com/springboot/blog/service/FileService.java
@@ -1,4 +1,4 @@
-package com.springboot.blog.service.impl;
+package com.springboot.blog.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
--- src/main/java/com/springboot/blog/service/impl/FileServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/FileServiceImpl.java
@@ -1,12 +1,13 @@
 package com.springboot.blog.service.impl;
 
+import com.springboot.blog.service.FileService;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class FileServiceImpl implements FileService{
+public class FileServiceImpl implements FileService {
     @Override
     public String uploadFile(String path, MultipartFile file) throws IOException {
         // get name of the file
