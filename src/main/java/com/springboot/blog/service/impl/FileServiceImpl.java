7ab4b888699db1e8d0e99180688ce7a24bd872e8
Deshmukh Danish <dd39775@gmail.com>
2024-06-26 11:53:36 +0530

    add functionality into the FileServiceImpl.class

--- src/main/java/com/springboot/blog/service/impl/FileServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/FileServiceImpl.java
@@ -3,19 +3,34 @@ package com.springboot.blog.service.impl;
 import com.springboot.blog.service.FileService;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileServiceImpl implements FileService {
     @Override
     public String uploadFile(String path, MultipartFile file) throws IOException {
         // get name of the file
-        return null;
+        String fileName = file.getOriginalFilename();
+
+        // to get the file path
+        String filePath = path + File.separator + fileName;
+
+        // create file object
+        File f = new File(path);
+        if(!f.exists()) {
+            f.mkdir();
+        }
+
+        // copy the file or upload file to the path
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+
+        return fileName;
     }
 
     @Override
     public InputStream getResourceFile(String path, String fileName) throws FileNotFoundException {
-        return null;
+        String filePath = path + File.separator + fileName;
+        return new FileInputStream(filePath);
     }
 }
